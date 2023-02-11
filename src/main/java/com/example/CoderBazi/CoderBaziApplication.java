60f20bc575eb4fa490c27bfc2fa80a02f25b66c5
pratik jain <pratikpjain.15@gmail.com>
2023-02-11 21:07:11 +0530

    creating the project

---
+++ src/main/java/com/example/CoderBazi/CoderBaziApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoderBazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderBaziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderBaziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CoderBazi/CoderBaziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CoderBazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderBaziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
