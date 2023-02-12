a03e5af37b7f0e60329720b41a16c9434aa17f54
pratik jain <pratikpjain.15@gmail.com>
2023-02-12 12:15:29 +0530

    added layers

---
+++ src/main/java/com/example/CoderBazi/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.example.CoderBazi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
---
+++ src/main/java/com/example/CoderBazi/entities/User.java
@@ -0,0 +1,5 @@
+package com.example.CoderBazi.entities;
+
+public class User {
+
+}
