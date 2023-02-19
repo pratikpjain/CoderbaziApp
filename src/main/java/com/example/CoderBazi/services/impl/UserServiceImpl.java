2194e18a1651c883334cb873d2ad4ce9f78e3791
Pratik Jain <63181338+pratikpjain@users.noreply.github.com>
2023-02-20 00:20:20 +0530

    ✔️add validations for add user api

--- src/main/java/com/example/CoderBazi/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/CoderBazi/services/impl/UserServiceImpl.java
@@ -24,6 +24,10 @@ public class UserServiceImpl implements UserService {
     }
     @Override
     public Response addUser(String name, String userName, String phoneNumber) {
+        String ValidationResponse = ValidateUser(name, userName, phoneNumber);
+        if(ValidationResponse != null) {
+            return new Response(401, ValidationResponse, null);
+        }
         if(UserWithUserNameExists(userName)) {
             return new Response(401, "User with this user-name already exists! Please try another user-name.", null);
         }
@@ -40,8 +44,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public Response getUsers() {
         List<User> userList = usersRepository.getUsers();
-
-        return new Response(200, "Here are the users", userList);
+        int numberOfUsers = userList.size();
+        return new Response(200, "There are total " + numberOfUsers + " users in our database", userList);
     }
 
     @Override
@@ -49,6 +53,49 @@ public class UserServiceImpl implements UserService {
         User deletedUser = usersRepository.getUserByUserName(userName);
         usersRepository.deleteAllByUserName(userName);
         return new Response(200, "Users Deleted successfully", deletedUser);
+    }
+    String ValidateUser(String name, String userName, String phoneNumber) {
+
+        String validateName = ValidateName(name);
+        if (validateName != null) return validateName;
+        String validateUserName = ValidateUserName(userName);
+        if (validateUserName != null) return validateUserName;
+        String validatePhoneNumber = ValidatePhoneNumber(phoneNumber);
+        if (validatePhoneNumber != null) return validatePhoneNumber;
+
+        return null;
+    }
+
+    String ValidateName(String name) {
+        if(name == "") return "Name field cannot be empty";
+        if(name.length() >= 100) return "Name must not exceed 100 characters";
+        return null;
+    }
+    String ValidateUserName(String userName) {
+        if(userName.length() < 3 || userName.length() > 20) return "user-name length must be between 3 to 20.";
+        for(int i = 0 ; i < userName.length() ; ++i) {
+            char c = userName.charAt(i);
+            if(!(isAlphabet(c) || isNumber(c) || isSpecial(c))) return "user-name must contain alphabets, number, '.' and '_' only. No other characters are allowed!";
+        }
+        return null;
+    }
+    String ValidatePhoneNumber(String phoneNumber) {
+        if(phoneNumber.length() != 10) return "phone-number must be of length 10";
+        for(int i = 0 ; i < 10 ; ++i) {
+            char c = phoneNumber.charAt(i);
+            if(i == 0 && c == '0') return "First digit of number should not be zero";
+            if(!isNumber(c)) return "Phone number should contain only numerical values";
+        }
+        return null;
+    }
+    boolean isAlphabet(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+    boolean isNumber(char c) {
+        return (c >= '0' && c <= '9');
+    }
+    boolean isSpecial(char c) {
+        return c == '.' || c == '_';
     }
     private boolean UserWithUserNameExists(String userName) {
         if(usersRepository.UserWithUserNameExists(userName) > 0) return true;
