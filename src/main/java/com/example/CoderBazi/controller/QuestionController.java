c20045e909aa4b8a1c05869ed3c586ccaea03429
Pratik Jain <63181338+pratikpjain@users.noreply.github.com>
2023-02-22 18:17:37 +0530

    adding health check api

--- src/main/java/com/example/CoderBazi/controller/QuestionController.java
+++ src/main/java/com/example/CoderBazi/controller/QuestionController.java
@@ -2,9 +2,8 @@ package com.example.CoderBazi.controller;
 
 import com.example.CoderBazi.entities.Question;
 import com.example.CoderBazi.payload.Response;
-import com.example.CoderBazi.payload.ResponseFile;
 import com.example.CoderBazi.services.QuestionService;
-import com.example.CoderBazi.services.UserService;
+import com.j256.simplemagic.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -13,8 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.io.ByteArrayInputStream;
 
 @RestController
@@ -25,7 +22,6 @@ public class QuestionController {
     public QuestionController(QuestionService questionService) {
         this.questionService = questionService;
     }
-
     @PostMapping("addQuestion/")
     public ResponseEntity<Response> AddQuestion(@RequestParam(value = "user-name") String userName,
                                                 @RequestParam(value = "file")MultipartFile file) {
@@ -35,17 +31,20 @@ public class QuestionController {
             return new ResponseEntity<>(new Response(401, e.getMessage(), null), HttpStatus.BAD_REQUEST);
         }
     }
-
     @GetMapping("getQuestion/")
     public ResponseEntity<Resource> GetQuestion(@RequestParam(value = "question-id") int questionId) {
         Question question = questionService.GetQuestion(questionId);
-        String fileName = "problem/"+questionId;
+        String fileName = "problem/"+questionId+".pdf";
         ByteArrayInputStream byteArrayOutputStream;
         byteArrayOutputStream = new ByteArrayInputStream(question.getFile());
         InputStreamResource fileInputStream = new InputStreamResource(byteArrayOutputStream);
         HttpHeaders headers = new HttpHeaders();
         headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
-        headers.set(HttpHeaders.CONTENT_TYPE, "application/pdf");
+        headers.set(HttpHeaders.CONTENT_TYPE, ContentType.PDF.getMimeType());
         return new ResponseEntity<>(fileInputStream, headers, HttpStatus.OK);
+    }
+    @DeleteMapping("deleteQuestion/")
+    public ResponseEntity<Response> DeleteQuestion(@RequestParam(value = "question-id") int questionId) {
+        return new ResponseEntity<>(questionService.DeleteQuestion(questionId), HttpStatus.OK);
     }
 }
